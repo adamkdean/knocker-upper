d8c7ea4413b238e6d6bc49a10e0c503245326974
Adam K Dean <adam@amberfish.co.uk>
2015-02-06 23:00:03 +0000

    Added basic functionality and example config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-var config = require('../config/apps');
+var http = require('http'),
+    apps = require('../config/apps');
 
-console.log(config);
+var defaults = {
+    port: 80,
+    path: '/',
+    method: 'HEAD',
+    interval: 15*60*1000 // 15 minutes
+}
+
+// loop through all configured apps, and setup their interval
+for(var i = 0; i < apps.length; i++) {
+    (function(host, port, path, method, interval) {
+        setInterval(function() {
+            knockUp(host, port, path, method);
+        }, interval);
+    }(generateHerokuUrl(apps[i].host),
+      apps[i].port || defaults.port,
+      apps[i].path || defaults.path,
+      apps[i].method || defaults.method,
+      apps[i].interval || defaults.interval));
+}
+
+function generateHerokuUrl(host) {
+    return (host.indexOf('herokuapp.com') === -1)
+        ? host + '.herokuapp.com'
+        : host;
+}
+
+function knockUp(host, port, path, method) {
+    var options = {
+        host: host,
+        port: port,
+        path: path,
+        method: method
+    };
+
+    var req = http.request(options, function (res) {
+        console.log(host + ': request successful');
+    });
+
+    req.on('error', function (err) {
+        console.log(host + ': error', err);
+    });
+
+    req.on('timeout', function () {
+        console.log(host + ': connection timeout');
+        req.abort();
+    });
+
+    req.end();
+}
